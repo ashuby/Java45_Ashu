3c70322f149e2a023847fcd5a3f133685da7dab3
Ashutosh Singh (CW) <Ashutosh.Singh@jda.com>
2024-01-29 19:01:43 +0530

    Delete and FindAll functions are added

---
+++ SpringJPAHibernate/src/main/java/StudentApplication/SpringJPAHibernate/Controllers/UserController.java
@@ -0,0 +1,43 @@
+package StudentApplication.SpringJPAHibernate.Controllers;
+
+import StudentApplication.SpringJPAHibernate.Model.User;
+import StudentApplication.SpringJPAHibernate.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserRepository userRepoObj;
+    @PostMapping("/signup")
+    public User save(@RequestBody User userObj){
+        System.out.println(userObj);
+        User userResponse = userRepoObj.save(userObj);
+
+        return userResponse;
+    };
+
+    @PostMapping("/del")
+    public void delete(@RequestBody User userObj1 ) {
+        System.out.println(userObj1);
+
+        userRepoObj.delete(userObj1);
+    };
+
+    @PostMapping("/find")
+    public List<User> findAll() {
+
+        List<User> userResponse = userRepoObj.findAll();
+        return  userResponse;
+    };
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello!!!";
+    }
+
+}
+
